0c374b0126be6adf6b93cf6395336515e955c78b
hangeulBae <dxdiag20@gmail.com>
2018-10-01 22:28:30 +0900

    spring set

--- src/main/java/team1/App.java
+++ src/main/java/team1/App.java
@@ -4,10 +4,27 @@ package team1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class App {
+    @Value("${environment.message}")
+    private String message;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World! This is " + message ;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("test");
+        System.out.println("test2");
+        SpringApplication.run(App.class, args);
     }
 }
