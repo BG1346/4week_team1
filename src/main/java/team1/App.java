3776d7f1f5eaceda42e21bba1df115938981fe4d
Bong Geun Shin <bg1346@naver.com>
2018-10-03 00:50:50 +0900

    calculator revised

--- src/main/java/team1/App.java
+++ src/main/java/team1/App.java
@@ -4,38 +4,20 @@ package team1;
 
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-	System.out.println("두개의 숫자를 입력받아 계산합니다. 첫번째 숫자를 입력해주세요");
-
-	int num1 = input.nextInt();
+       System.out.println("두개의 숫자를 계산하는 계산기 입니다.");
+	int num1 = (int)(10*Math.random());
+	int num2 = (int)(10*Math.random());
+	System.out.println("ex)" + num1 + " + , - " + num2);
 
 	System.out.println("연산자를 입력해주세요. (+, -, *, / 중 하나)");
 
-	int operator = input.nextInt();
-
 	System.out.println("두번째 숫자를 입력해주세요.");
 
-	int num2 = input.nextInt();
 
 	
-	   if(operator == '-'){
-	   int result = num1 - num2;
-	   System.out.println("두 수의 빼기는"+result);
-	  }
-	  else if(operator == '+'){
 	   int result = num1 + num2;
-	   System.out.println("두 수의 합은"+result);
-	  }
-	  else
-   	  {
-	   System.out.println("잘못된 입력입니다.");
-	  }
-
-
-
-
-
-
+	   System.out.println("두 수의 빼기는"+num1+"+"+num2+"="+result);
+	   result = num1 - num2;
+	   System.out.println("두 수의 합은"+num1+"-"+num2+"="+result);
     }
 }
