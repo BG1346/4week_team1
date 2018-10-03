b9d6ba647c04c78daa5670e13b757dd378798701
BG1346 <bg1346@naver.com>
2018-10-03 16:40:55 +0900

    revised for integration

--- src/main/java/team1/App.java
+++ src/main/java/team1/App.java
@@ -9,26 +9,20 @@ public class App {
       user1.setName("jason");
       user1.setAge(29);
       user1.setYears("senior");
-      System.out.println(user1.getName()+" "+user1.getAge()+" "+user1.getYears());
-      
+      System.out.println(user1.getName()+" "+user1.getAge()+" "+user1.getYears());      
       // 계산기
-      System.out.println("두개의 숫자를 계산하는 계산기 입니다.");
-      int num1 = (int)(10*Math.random());
-      int num2 = (int)(10*Math.random());
-      System.out.println("ex)" + num1 + " + , - " + num2);
-      System.out.println("연산자를 입력해주세요. (+, -, *, / 중 하나)");
-      System.out.println("두번째 숫자를 입력해주세요.");
-	   int result = num1 + num2;
-	   System.out.println("두 수의 빼기는"+num1+"+"+num2+"="+result);
-	   result = num1 - num2;
-	   System.out.println("두 수의 합은"+num1+"-"+num2+"="+result);
-     if(input4.equals("*")){
-          int result = num * num2;
-          System.out.println("두 수의 곱은"+result);
-      }
-      else if(input4.equals("/")){
-          int result = num / num2;
-          System.out.println("두 수의 나누기는"+result);
+      System.out.println("10 미만의 임의 2개의 숫자를 계산하는 계산기 입니다.");
+      int num1 = (int)(9*Math.random()+1);
+      int num2 = (int)(9*Math.random()+1);
+      System.out.println("ex)" + num1 + " + , -, *, / " + num2);
+      int result = num1 + num2;
+      System.out.println("두 수의 덧셈은" +num1+"+"+num2+"="+result);
+      result = num1 - num2;
+      System.out.println("두 수의 뺄셈은"+num1+"-"+num2+"="+result);
+      result = num1 * num2;
+      System.out.println("두 수의 곱셈은"+num1+"*"+num2+"="result);
+      reulst = num1 / num2;
+      System.out.println("두 수의 나눗셈은"+num1+"/"+num2+"="+result);
     }
 }
 
