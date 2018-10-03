af6d9754f5352742e10126e22a8e534a04e26cd5
hangeulBae <dxdiag20@gmail.com>
2018-10-04 06:45:40 +0900

    app.java package error fix, code error fix

--- src/test/java/team1/AppTest.java
+++ src/test/java/team1/AppTest.java
@@ -1,26 +1,14 @@
 package team1;
-
 import java.util.Scanner;
 import org.junit.Test;
-
-
-public class AppTest {
-    package team1;
-
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-
+public class AppTest {
     /**
      * Unit test for simple App.
      */
-    public class AppTest {
-        /**
-         * Rigorous Test :-)
-         */
-        @Test
-        public void shouldAnswerWithTrue() {
-            assertTrue(true);
-        }
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
